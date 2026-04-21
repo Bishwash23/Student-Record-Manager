16afbc2a48321073b40a2a846fb24db432098821
Bishwash23 <bishwashb23@gmail.com>
2026-04-21 12:21:38 +0545

    Enhance student management functionality: add update and delete operations, improve search logic

--- student.py
+++ student.py
@@ -58,13 +58,41 @@ def add_student():
 def search_student():
     student_id = input("Student ID: ")
     data = load()
-    for item in data:
-        if item["student_id"] == student_id:
-            name = item["name"]
-            marks = item["marks"]
-            print("\nStudent details\n")
-            print(f"{student_id} | {name} | {marks}")
+    student = next((item for item in data if item["student_id"] == student_id), None)
+    if student:
+        print("\nStudent details\n")
+        print(f"{student_id} | {student["name"]} | {student["marks"]}\n")
+    else:
+        print("Student not found")
 
+def update_marks():
+    student_id = input("Student ID: ")
+    data = load()
+    student = next((item for item in data if item["student_id"] == student_id), None)
+    if student:
+        print("\nStudent details\n")
+        print(f"{student_id} | {student["name"]} | {student["marks"]}\n")
+        try:
+            marks = int(input("New marks: "))
+        except ValueError:
+            print("Invalid marks")
+            return
+        student["marks"] = marks
+        save(data)
+        print(f"\nUpdated: {student_id} | {student["name"]} | {student["marks"]}\n")
+    else:
+        print("Student not found")
+
+def delete_record():
+    student_id = input("Student ID: ")
+    data = load()
+    new_data = [item for item in data if item["student_id" != student_id]]
+    if len(new_data) == len(data):
+        print("Student not found")
+        return
+    else:
+        save(new_data)
+        print("Student deleted successfully")
 
 
 
